dcf439c06553abe889d60dd155e91adaeb5017e3
bripo-mango <jesusgreenn@gmail.com>
2021-02-08 22:54:51 -0300

    initial commit

---
+++ listas/listas.js
@@ -0,0 +1,13 @@
+function adddata(){
+
+    var name = document.getElementById("name").value;
+    var apellido = document.getElementById("apell").value;
+    var documento = parseInt(document.getElementById("docu").value);
+    var fecha = document.getElementById("date").value;
+    var genero = document.getElementById("gender").value;
+    var nacionalidad = document.getElementById("nacio").value;
+
+    document.getElementById("tablebody").innerHTML += "<tr> <td>"+name+"</td> <td>"+apellido+"</td> <td>"+documento+"</td> <td>"+fecha+"</td> <td>"+nacionalidad+" </td> <td>"+genero+"</td> </tr>";
+
+
+}
