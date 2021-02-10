001493e7e3ae177cd8f82601b340ea3bf41a1857
salibacristian <chriseze21@gmail.com>
2021-02-09 22:58:46 -0300

    agregar a la lista el usuario en la funcion adddata

--- listas/listas.js
+++ listas/listas.js
@@ -1,14 +1,31 @@
+var userList = [];//declaro el array
+
 function adddata(){
 
-    var name = document.getElementById("name").value;
+    //1. tomo los datos del formulario
+    var nameFromHtml = document.getElementById("name").value;
     var apellido = document.getElementById("apell").value;
     var documento = parseInt(document.getElementById("docu").value);
     var fecha = document.getElementById("date").value;
     var genero = document.getElementById("gender").value;
     var nacionalidad = document.getElementById("nacio").value;
 
+    //2. creo un objeto con los datos tomados previamente y agregar a la lista
+
+    var obj = {
+        name: nameFromHtml,
+        lastname: apellido,
+        id: documento,
+        date: fecha,
+        gender: genero,
+        country: nacionalidad        
+    };
+
+    userList.push(obj);
+
+    //3. escribo en el tableBody
     document.getElementById("tablebody").innerHTML += 
-    "<tr> <td>"+name
+    "<tr> <td>"+nameFromHtml
     +"</td> <td>"+apellido
     +"</td> <td>"+documento
     +"</td> <td>"+fecha
@@ -16,5 +33,6 @@ function adddata(){
     +" </td> <td>"+genero
     +" </td> <td> <i class='fa fa-trash'></i> </td> </tr>";
 
-
 }
+
+
