d80a887cdcf14fdb63f12b94e787db8d88e9dee0
salibacristian <chriseze21@gmail.com>
2021-02-09 23:26:09 -0300

    baja

--- listas/listas.js
+++ listas/listas.js
@@ -1,6 +1,6 @@
 var userList = [];//declaro el array
 
-function adddata(){
+function adddata() {
 
     //1. tomo los datos del formulario
     var nameFromHtml = document.getElementById("name").value;
@@ -18,21 +18,51 @@ function adddata(){
         id: documento,
         date: fecha,
         gender: genero,
-        country: nacionalidad        
+        country: nacionalidad
     };
 
     userList.push(obj);
 
-    //3. escribo en el tableBody
-    document.getElementById("tablebody").innerHTML += 
-    "<tr> <td>"+nameFromHtml
-    +"</td> <td>"+apellido
-    +"</td> <td>"+documento
-    +"</td> <td>"+fecha
-    +"</td> <td>"+nacionalidad
-    +" </td> <td>"+genero
-    +" </td> <td> <i class='fa fa-trash'></i> </td> </tr>";
+    updateList();
+}
+
+function deleteUser(userId) {
+    //busca al usuario dentro de la lista por id y lo borra
+    //para borrar un elemento del array usar la funcion splice
+    //como splice pide un indice del array primero tenemos que obtenerlo
+    let index;
+
+    for (index = 0; index < userList.length; index++) {
+        if (userList[index].id == userId) {
+            //lo encontro, me quedo con el indice, freno
+            break;
+        }
+    }
+
+    userList.splice(index, 1);
+
+    updateList();
 
 }
 
+function updateList() {
+    //1. VACIO LA TABLA
 
+    document.getElementById("tablebody").innerHTML = "";
+
+    //2. recorro la lista y voy escribiendo tr
+
+    userList.forEach(function (user) {
+        document.getElementById("tablebody").innerHTML +=
+            "<tr> <td>" + user.name
+            + "</td> <td>" + user.lastname
+            + "</td> <td>" + user.id
+            + "</td> <td>" + user.date
+            + "</td> <td>" + user.country
+            + " </td> <td>" + user.gender
+            + " </td> <td> <a onclick = 'deleteUser(" + user.id + ")'> <i class='fa fa-trash'></i> </a> </td> </tr>";
+    });
+
+
+
+}
